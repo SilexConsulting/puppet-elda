4f5405ee2e757ceb4aafe41f017f00850a76e355
Jason Brown <jason@silex-consulting.com>
2014-09-08 18:40:01 +0100

    Update install.pp to use standalone package then symlink the webapp.

--- manifests/install.pp
+++ manifests/install.pp
@@ -35,6 +35,17 @@ class elda::install(){
   file { $::elda::params::install_path:
     ensure    => directory,
     recurse   => true,
+    purge     => false,
+    force     => false,
+    owner     => $::elda::params::owner,
+    group     => $::elda::params::group,
+    mode      => 766,
+  }
+
+  #Ensure the Log folder is created according to log_path parameter
+  file { $::elda::params::log_path:
+    ensure    => directory,
+    recurse   => true,
     purge     => true,
     force     => true,
     owner     => $::elda::params::owner,
@@ -42,30 +53,50 @@ class elda::install(){
     mode      => 766,
   }
 
-  # Symlink the Log path under the install_path ?
+  # Symlink the log path under the install_path.
   file { $::elda::params::log_symlink:
     ensure => 'link',
     force  => true,
     target => $::elda::params::log_path,
+    require => File["${::elda::params::log_path}"],
   }
 
-  #Download the Elda API front end archive.
-  exec { "download-elda-jar":
-    command   => "curl -4sv ${::elda::params::download_url}${::elda::params::elda_package_name} > ${::elda::params::install_path}/elda.jar",
+  #Download the Elda API front end archive SEE STANDALONE OR CONTAINER IN params.pp .
+  exec { "download-elda":
+    command   => "curl -4sv ${::elda::params::download_url}${::elda::params::elda_package_name} > ${::elda::params::install_path}/${::elda::params::elda_package_name}",
     path      => '/usr/bin',
-    onlyif    => "test ! -f ${::elda::params::install_path}/elda.jar",
+    onlyif    => "test ! -f ${::elda::params::install_path}/${::elda::params::elda_package_name}",
     require   => File[$::elda::params::install_path],
   }
 
-  #Load Jar file into tomcat webapps directory.
+  #Ensure a directory under tomcat webapps is created and filled with content.
+  file {"${::elda::params::tomcat_webapps}/${::elda::params::elda_tomcat_app_name}":
+    ensure    => directory,
+    recurse   => true,
+    purge     => false,
+    force     => false,
+    owner     => $::elda::params::owner,
+    group     => $::elda::params::group,
+    mode      => 766,
+    source    => "${::elda::params::install_path}/webapps/standalone",
+    require   => Exec['extract-elda'],
+  }
 
-
-  exec { 'extract-elda-jar':
+  #Extract archive into the install directory.
+  exec { 'extract-elda':
     path      => ['/usr/bin', '/usr/sbin', '/bin'],
-    cwd       => $::elda::params::install_path,
-    command   => "jar xvf ${::elda::params::install_path}/elda.jar",
-    onlyif    => "test -f ${::elda::params::install_path}/elda.jar",
-    require   => Exec["download-elda-jar"]
+    cwd       => "${::elda::params::install_path}",
+    command   => "jar xf ${::elda::params::elda_package_name}",
+    onlyif    => "test -f ${::elda::params::install_path}/${::elda::params::elda_package_name}",
+    require   => Exec['download-elda'],
+  }
+
+  # Symlink the standalone webapps path under the tomcat webapps path.
+    file { "${::elda::params::tomcat_webapps_path}/${::elda::params::elda_tomcat_app_name}" :
+    ensure => 'link',
+    force  => true,
+    target => "${::elda::params::install_path}/webapps/standalone",
+    require => File["${::elda::params::install_path}"],
   }
 
   #Temp need to add to tomcat webapps instead of standalone.

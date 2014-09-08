ec238198856e5e6155280dfffffe5afe69133094
Jason Brown <jason@silex-consulting.com>
2014-09-08 18:38:11 +0100

    Add to elda parameters.

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,10 +17,13 @@ class elda::params(
     $variable_path = '/var/lib/elda',
     $temp_path = '/tmp',
     $log_symlink = '/usr/share/elda/logs',
-    $log_path  = '/var/log/elda',
-    $elda_package_name = 'organogram.war',
-    #$download_url = 'http://repository.epimorphics.com/com/epimorphics/lda/elda-standalone/1.2.35/',
-    $download_url = 'https://s3-eu-west-1.amazonaws.com/organograms/RELEASE-0.1/',
+    $log_path = '/var/log/elda',
+    $elda_package_name = 'elda-standalone-1.2.35.jar',
+    $download_url = 'http://repository.epimorphics.com/com/epimorphics/lda/elda-standalone/1.2.35/',
+    #$elda_package_name = 'organogram.war',
+    #$download_url = 'https://s3-eu-west-1.amazonaws.com/organograms/RELEASE-0.1/',
+    $tomcat_webapps_path = '/var/lib/tomcat7/webapps',
+    $elda_tomcat_app_name = "elda-home",
   )
   {
     

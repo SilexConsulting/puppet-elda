d6d52f73e4e68fe5d8ee8e13664dd6881081a5b3
Jason Brown <jason@silex-consulting.com>
2014-09-08 13:14:09 +0100

    Use download url for .war file instead of standalone jar.

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,9 +17,10 @@ class elda::params(
     $variable_path = '/var/lib/elda',
     $temp_path = '/tmp',
     $log_symlink = '/usr/share/elda/logs',
-    $log_pathls  = '/var/log/elda',
-    $elda_package_name = 'elda-standalone-1.2.35.jar',
-    $download_url = 'http://repository.epimorphics.com/com/epimorphics/lda/elda-standalone/1.2.35/',
+    $log_path  = '/var/log/elda',
+    $elda_package_name = 'organogram.war',
+    #$download_url = 'http://repository.epimorphics.com/com/epimorphics/lda/elda-standalone/1.2.35/',
+    $download_url = 'https://s3-eu-west-1.amazonaws.com/organograms/RELEASE-0.1/',
   )
   {
     

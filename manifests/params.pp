97312358cc1eea7ea3bd00e44c4447b72678b877
Jason Brown <jason@silex-consulting.com>
2014-09-09 11:25:45 +0100

    Update params so hiera can inject data.

--- manifests/params.pp
+++ manifests/params.pp
@@ -11,20 +11,19 @@
 # Sample Usage:
 #
 class elda::params(
-    $owner = 'elda',
-    $group = 'elda',
-    $install_path = '/usr/share/elda',
-    $variable_path = '/var/lib/elda',
-    $temp_path = '/tmp',
-    $log_symlink = '/usr/share/elda/logs',
-    $log_path = '/var/log/elda',
-    $elda_package_name = 'elda-standalone-1.2.35.jar',
-    $download_url = 'http://repository.epimorphics.com/com/epimorphics/lda/elda-standalone/1.2.35/',
-    #$elda_package_name = 'organogram.war',
-    #$download_url = 'https://s3-eu-west-1.amazonaws.com/organograms/RELEASE-0.1/',
-    $tomcat_webapps_path = '/var/lib/tomcat7/webapps',
-    $elda_tomcat_app_name = "elda-home",
-  )
-  {
-    
+  $install_path = '/usr/share/elda',
+  $temp_path = '/tmp',
+  $log_symlink = '/usr/share/elda/logs',
+  $log_path = '/var/log/elda',
+  $elda_package_name = 'organogram.war',
+  $download_url = 'https://s3-eu-west-1.amazonaws.com/organograms/RELEASE-0.1/',
+  $tomcat_webapps_path = "/var/lib/tomcat${tomcat::params::version}",
+) {
+  case $::osfamily {
+      'Debian': {
+    }
+    default: {
+      fail("${::operatingsystem} not supported")
+    }
   }
+}
